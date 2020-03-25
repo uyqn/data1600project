ce660c073a42929f87ff83053f2440e16cbd735f
AndreasTorres <andreas@thehumanaspect.com>
2020-03-25 13:43:57 +0100

    hello

--- src/main/java/components/Component.java
+++ src/main/java/components/Component.java
@@ -1,5 +1,48 @@
 package components;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Component {
+
+    private SimpleStringProperty name, manufacturer, model;
+    private SimpleDoubleProperty price;
+
+    public String getName() {
+        return name.get();
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getManufacturer() {
+        return manufacturer.get();
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer.set(manufacturer);
+    }
+
+    public String getModel() {
+        return model.get();
+    }
+
+    public void setModel(String model) {
+        this.model.set(model);
+    }
+
+    public double getPrice() {
+        return price.get();
+    }
+
+    public void setPrice(double price) {
+        this.price.set(price);
+    }
+
+    @Override
+    public String toString(){
+        return "Komponent" + name +":";
+    }
 
 }
