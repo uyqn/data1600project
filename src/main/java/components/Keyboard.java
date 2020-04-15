cc3b46a6b327ace03c4916d37d91998a20dfe486
AndreasTorres <andreas@thehumanaspect.com>
2020-04-15 12:35:15 +0200

    toCSV på powersupply

--- src/main/java/components/Keyboard.java
+++ src/main/java/components/Keyboard.java
@@ -1,14 +1,39 @@
 package components;
 
+import fileManager.Formatter;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Keyboard extends Component {
 
+    private transient static final SimpleStringProperty COMPONENT_TYPE = new SimpleStringProperty("Keyboard");
+    private SimpleBooleanProperty Tactile;
 
-    public Keyboard(String manufacturer, String model, double price) {
+    public Keyboard(String manufacturer, String model, double price, boolean Tactile) {
         super(manufacturer, model, price);
+
+        setTactile(Tactile);
+    }
+
+    public boolean isTactile() {
+        return Tactile.get();
+    }
+
+    public void setTactile(boolean tactile) {
+        this.Tactile.set(tactile);
+    }
+
+    public static String getComponentType(){
+        return COMPONENT_TYPE.get();
     }
 
     @Override
     public String toCSV() {
-        return null;
+        return Formatter.toCSV(getComponentType(),
+                getManufacturer(),
+                getModel(),
+                isTactile(),
+                getPrice()
+        );
     }
 }
