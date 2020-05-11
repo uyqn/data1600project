12a0399ae20d5a654fd4ccbad5e6dc9e9b5c6c32
AndreasTorres <andreas@thehumanaspect.com>
2020-05-11 12:23:44 +0200

    store

--- src/main/java/controllers/user/EndUserController.java
+++ src/main/java/controllers/user/EndUserController.java
@@ -1,12 +1,19 @@
 package controllers.user;
 
+import controllers.component.ComponentController;
 import controllers.guiManager.GUI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import controllers.LoginController;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 import users.User;
+import javafx.scene.Node;
 
 import java.io.IOException;
 
@@ -25,13 +32,30 @@ public class EndUserController{
     private Label lblWelcome;
 
     @FXML
-    void buildPc(ActionEvent event) {
-
-
-    }
+    private GridPane endHome;
 
     @FXML
-    void showPrev(ActionEvent event) {
+    void buildPc(ActionEvent event) throws IOException {
+            Parent view = FXMLLoader.load(getClass().getResource("buildPc.fxml"));
+
+            Scene scene = new Scene(view);
+
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            window.setScene(scene);
+            window.show();
+
+        }
+
+
+    @FXML
+    void showPrev(ActionEvent event) throws IOException {
+        Parent view = FXMLLoader.load(getClass().getResource("prevPC.fxml"));
+
+        Scene scene = new Scene(view);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
 
     }
 
--- src/main/java/controllers/user/endUsers/PrevPcController.java
+++ src/main/java/controllers/user/endUsers/PrevPcController.java
@@ -1,5 +1,48 @@
 package controllers.user.endUsers;
+
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PrevPcController {
+
+    @FXML
+    private TableView<?> tableView;
+
+    @FXML
+    private Button homeBtn;
+
+    @FXML
+    private Button addBtn;
+
+    @FXML
+    void addPc(ActionEvent event) throws IOException {
+        Parent view = FXMLLoader.load(getClass().getResource("buildPc.fxml"));
+
+        Scene scene = new Scene(view);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+
+    @FXML
+    void backHome(ActionEvent event) throws IOException {
+        Parent view = FXMLLoader.load(getClass().getResource("endUser.fxml"));
+
+        Scene scene = new Scene(view);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+
 }
