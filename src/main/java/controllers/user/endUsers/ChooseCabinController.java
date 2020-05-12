44d6411f0ed20d89b105f78e1af380bb7a9d6071
AndreasTorres <andreas@thehumanaspect.com>
2020-05-12 21:21:36 +0200

    store

--- src/main/java/controllers/user/endUsers/ChooseCabinController.java
+++ src/main/java/controllers/user/endUsers/ChooseCabinController.java
@@ -1,24 +1,65 @@
 package controllers.user.endUsers;
 
+import components.Cabin;
 import components.Component;
+import components.GPU;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import main.App;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
-public class ChooseCabinController {
+public class ChooseCabinController implements Initializable {
 
     @FXML
     private TableView<Component> tableView;
 
+    @FXML
+    private TableColumn<Cabin, String> ManufacturerColumn;
 
+    @FXML
+    private TableColumn<Cabin, String> ModelColumn;
+
+    @FXML
+    private TableColumn<Cabin, Double> PriceColumn;
+
+    @FXML
+    private TableColumn<Cabin, String> FormFactorColumn;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+        //Setter opp kolonner
+
+        ManufacturerColumn.setCellValueFactory(new PropertyValueFactory<Cabin, String>("manufacturer"));
+        ModelColumn.setCellValueFactory(new PropertyValueFactory<Cabin, String>("model"));
+        PriceColumn.setCellValueFactory(new PropertyValueFactory<Cabin, Double>("price"));
+        FormFactorColumn.setCellValueFactory(new PropertyValueFactory<Cabin, String>("formFactor"));
+
+
+
+        tableView.setItems(
+                App.componentList.getList().stream().filter(component ->
+                        component.getComponentType().equals(Cabin.COMPONENT_TYPE)
+                ).collect(Collectors.toCollection(FXCollections::observableArrayList))
+        );
+
+    }
 
     @FXML
     private Button backBtn;
--- src/main/java/controllers/user/endUsers/ChooseKeyboardController.java
+++ src/main/java/controllers/user/endUsers/ChooseKeyboardController.java
@@ -1,22 +1,64 @@
 package controllers.user.endUsers;
 
+import components.Component;
+import components.Keyboard;
+import components.Storage.SSD;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import main.App;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
-public class ChooseKeyboardController {
+public class ChooseKeyboardController implements Initializable {
 
     @FXML
-    private TableView<?> tableView;
+    private TableView<Component> tableView;
 
+    @FXML
+    private TableColumn<Keyboard, String> ManufacturerColumn;
+
+    @FXML
+    private TableColumn<Keyboard, String> ModelColumn;
+
+    @FXML
+    private TableColumn<Keyboard, Double> PriceColumn;
+
+    @FXML
+    private TableColumn<Keyboard, Boolean> TactileColumn;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+        //Setter opp kolonner
+
+        ManufacturerColumn.setCellValueFactory(new PropertyValueFactory<Keyboard, String>("manufacturer"));
+        ModelColumn.setCellValueFactory(new PropertyValueFactory<Keyboard, String>("model"));
+        PriceColumn.setCellValueFactory(new PropertyValueFactory<Keyboard, Double>("price"));
+        TactileColumn.setCellValueFactory(new PropertyValueFactory<Keyboard, Boolean>("tactile"));
+
+
+        tableView.setItems(
+                App.componentList.getList().stream().filter(component ->
+                        component.getComponentType().equals(Keyboard.COMPONENT_TYPE)
+                ).collect(Collectors.toCollection(FXCollections::observableArrayList))
+        );
+
+    }
     @FXML
     private Button backBtn;
 
