46915484fe0a4d01a6c4616bac4f79d0c4d5e057
Helene <nostet@gmail.com>
2020-05-12 13:28:16 +0200

    Prevpc

--- src/main/java/controllers/user/endUsers/PrevPcController.java
+++ src/main/java/controllers/user/endUsers/PrevPcController.java
@@ -1,21 +1,47 @@
 package controllers.user.endUsers;
 
+import components.Computer;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import main.App;
 
 import java.io.IOException;
+import java.io.Serializable;
+import java.net.URL;
+import java.nio.MappedByteBuffer;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class PrevPcController {
+class ComputerRegister implements Serializable{
+    private transient ObservableList<Computer> computers= FXCollections.observableArrayList();
+
+    public List<Computer> getRegister(){return computers;}
+
+    public void attatchTableView(TableView tv){tv.setItems(computers);}
+
+
+}
+public class PrevPcController<T> implements Initializable {
+
+
+    private TableView<Computer> computerView=new TableView<>();
 
     @FXML
-    private TableView<?> tableView;
+    private GridPane viewComputersGui;
 
     @FXML
     private Button homeBtn;
@@ -44,5 +70,46 @@ public class PrevPcController {
         window.setScene(scene);
         window.show();
     }
+
+    private void getComputerView(){
+        TableColumn<Computer, String> nameCol = new TableColumn<>("Name");
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        computerView.getColumns().add(nameCol);
+
+        TableColumn<Computer, String> cpuCol = new TableColumn<>("CPU");
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("cpu"));
+        computerView.getColumns().add(cpuCol);
+
+        TableColumn<Computer, Double> priceCol = new TableColumn<>("Price");
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("price"));
+        computerView.getColumns().add(priceCol);
+
+
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        getComputerView();
+
+
+
+        viewComputersGui.add(computerView,1,1);
+
+
+
+    }
+   /* public class ComponentView<T>{
+
+
+        private TableView<Computer> computerView = new TableView<>();
+    }
+
+    private void getComputerView(){
+
+    }*/
+
+
+
 
 }
