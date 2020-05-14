11c9dbacd99fcb704fc702ffb17ec61774cd5a2a
Uy Quoc Nguyen <s341864@oslomet.no>
2020-05-15 01:05:35 +0200

    Finishing filemanagement for EndUSer

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -2,7 +2,7 @@ package main;
 
 import components.Component;
 import components.Computer;
-import fileManager.FileOpenerCSV;
+import fileManager.FileOpenerBin;
 import fileManager.FileSaverBin;
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -33,9 +33,9 @@ public class App extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
 
-        FileOpenerCSV tempOpener = new FileOpenerCSV();
+        FileOpenerBin tempOpener = new FileOpenerBin();
 
-        listableList.setList(tempOpener.open(Paths.get("temp.csv")));
+        listableList.setList(tempOpener.open(Paths.get("temp.bin")));
 
         stage.setOnCloseRequest(windowEvent -> {
             FileSaverBin<Component> tempSaver = new FileSaverBin<>();
